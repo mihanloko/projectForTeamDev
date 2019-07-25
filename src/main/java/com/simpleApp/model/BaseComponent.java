878e96824a9b4e14d18cb763c300f6cc8cbba173
Mikhail <rusmih98@yandex.ru>
2019-07-25 20:06:29 +0700

    В одном файле был lombok. сонарклауд

--- src/main/java/com/simpleApp/model/BaseComponent.java
+++ src/main/java/com/simpleApp/model/BaseComponent.java
@@ -1,18 +1,11 @@
 package com.simpleApp.model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
-@Getter
-@Setter
-@ToString
 public class BaseComponent {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
